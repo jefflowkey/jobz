def392f2f0c3e08aaffc75b6e6e3f34260c85774
jefflowkey <munyao.v.jm@gmail.com>
2022-12-02 00:07:39 +0300

    Undo -> Testing changes for fname, lname & email

--- signup.php
+++ signup.php
@@ -3,8 +3,8 @@
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$fname = $lname = $username = $email= $password = $confirm_password = "";
+$fname_err = $lname_err = $username_err = $email_err = $password_err = $confirm_password_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -67,14 +67,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (fname, lname, username, email, password) VALUES (?, ?)";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "ss", $param_fname, $param_lname, $param_username, $param_email, $param_password);
             
             // Set parameters
+            $param_fname = $fname;
+            $param_lname = $lname;
             $param_username = $username;
+            $param_email = $email;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             
             // Attempt to execute the prepared statement
